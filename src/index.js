7f700dc9a09fd93af11d0f277ebb64d2cb3697da
= <leandrotk100@gmail.com>
2021-04-17 18:39:05 -0300

    Add prettier

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import inquirer from 'inquirer'
+import inquirer from 'inquirer';
 
-inquirer.prompt([
-  {type: 'list', message: 'Bla:', name: 'thing', choices: [
-    'choice1',
-    'choice2',
-    'choice3',
-  ]}
-]).then(({thing}) => {
-  console.log(thing)
-})
+inquirer
+  .prompt([
+    {
+      type: 'list',
+      message: 'Bla:',
+      name: 'thing',
+      choices: ['choice1', 'choice2', 'choice3'],
+    },
+  ])
+  .then(({ thing }) => {
+    console.log(thing);
+  });
